9cdc649c24ed451d691987a0f62f889aa665915a
Aram Kocharyan <akarmenia@gmail.com>
2014-10-04 16:11:20 +1000

    Updated to Meteor 0.9.

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 // Meteor Package file.
 
 Package.describe({
-  name: 'pubsub',
-  summary: 'Topic-based publish/subscribe library'
+  name: 'aramk:pubsub',
+  summary: 'Topic-based publish/subscribe library',
+  git: "https://github.com/aramk/PubSubJS.git",
+  version: "1.5.0"
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1');
   api.use('jquery', 'client');
   api.export('PubSub', ['client', 'server']);
-  api.add_files([
+  api.addFiles([
     'src/pubsub.js'
   ], ['client', 'server']);
 });
